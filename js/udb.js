2a4b1f71515f46863729b44a30bc76c362908d5a
compleatang <compleatang@users.noreply.github.com>
2020-04-10 20:10:23 +0000

    deploy: 06511c9032af96f8a51f4409b01e2a62bea9c432

--- js/udb.js
+++ js/udb.js
@@ -5,13 +5,13 @@ var clicky_site_ids = clicky_site_ids || []; clicky_site_ids.push(101246300);
 
 function showMailingPopUp() {
   window.dojoRequire(["mojo/signup-forms/Loader"], function (L) { L.start({ "baseUrl": "mc.us19.list-manage.com", "uuid": "0c3e03a2482badc5c9c8958ab", "lid": "c82ae734c7", "uniqueMethods": true }) });
-  document.cookie = 'MCPopupClosed=;path=/;expires=Thu, 01 Jan 1970 00:00:00 UTC;';
-  document.cookie = 'MCPopupSubscribed=;path=/;expires=Thu, 01 Jan 1970 00:00:00 UTC;';
 };
 
 document.addEventListener("DOMContentLoaded", function (event) {
   document.querySelector('[title="Email"]').addEventListener("click", function (event) {
     event.preventDefault();
+    document.cookie = 'MCPopupClosed=;path=/;expires=Thu, 01 Jan 1970 00:00:00 UTC;';
+    document.cookie = 'MCPopupSubscribed=;path=/;expires=Thu, 01 Jan 1970 00:00:00 UTC;';
     showMailingPopUp();
   });
 });
