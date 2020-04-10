5bc9b6b2dd05437667a473c6afa03a540538ec4b
compleatang <compleatang@users.noreply.github.com>
2020-04-10 19:47:47 +0000

    deploy: c591ad066bba1486bc52889619785dfa4ff7fc7c

--- js/udb.js
+++ js/udb.js
@@ -1,1 +1,17 @@
+// clicky
 var clicky_site_ids = clicky_site_ids || []; clicky_site_ids.push(101246300);
+
+// subscriber embed
+
+function showMailingPopUp() {
+  window.dojoRequire(["mojo/signup-forms/Loader"], function (L) { L.start({ "baseUrl": "mc.us19.list-manage.com", "uuid": "0c3e03a2482badc5c9c8958ab", "lid": "c82ae734c7", "uniqueMethods": true }) });
+  document.cookie = "MCEvilPopupClosed=; expires=Thu, 01 Jan 1970 00:00:00 UTC";
+};
+
+document.addEventListener("DOMContentLoaded", function (event) {
+  document.querySelector('[title="Email"]').style.border = "thick solid red";
+  document.querySelector('[title="Email"]').addEventListener("click", function (event) {
+    event.preventDefault();
+    showMailingPopUp();
+  });
+});
