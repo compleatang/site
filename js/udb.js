bda05bb4bc4576e86548ebc7d44a93e650bcefd2
compleatang <compleatang@users.noreply.github.com>
2020-04-11 23:14:23 +0000

    deploy: 132a2f85e34ffff00944cfb628ff1c8a42c093ce

--- js/udb.js
+++ js/udb.js
@@ -2,16 +2,15 @@
 var clicky_site_ids = clicky_site_ids || []; clicky_site_ids.push(101246300);
 
 // subscriber embed
+// function showMailingPopUp() {
+//   window.dojoRequire(["mojo/signup-forms/Loader"], function (L) { L.start({ "baseUrl": "mc.us19.list-manage.com", "uuid": "0c3e03a2482badc5c9c8958ab", "lid": "c82ae734c7", "uniqueMethods": true }) });
+// };
 
-function showMailingPopUp() {
-  window.dojoRequire(["mojo/signup-forms/Loader"], function (L) { L.start({ "baseUrl": "mc.us19.list-manage.com", "uuid": "0c3e03a2482badc5c9c8958ab", "lid": "c82ae734c7", "uniqueMethods": true }) });
-};
-
-document.addEventListener("DOMContentLoaded", function (event) {
-  document.querySelector('a[title="Email"]').addEventListener("click", function (event) {
-    event.preventDefault();
-    document.cookie = 'MCPopupClosed=;path=/;expires=Thu, 01 Jan 1970 00:00:00 UTC;';
-    document.cookie = 'MCPopupSubscribed=;path=/;expires=Thu, 01 Jan 1970 00:00:00 UTC;';
-    showMailingPopUp();
-  });
-});
+// document.addEventListener("DOMContentLoaded", function (event) {
+//   document.querySelector('a[title="Email"]').addEventListener("click", function (event) {
+//     event.preventDefault();
+//     document.cookie = 'MCPopupClosed=;path=/;expires=Thu, 01 Jan 1970 00:00:00 UTC;';
+//     document.cookie = 'MCPopupSubscribed=;path=/;expires=Thu, 01 Jan 1970 00:00:00 UTC;';
+//     showMailingPopUp();
+//   });
+// });
